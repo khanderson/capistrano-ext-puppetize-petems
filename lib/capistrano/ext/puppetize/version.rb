0003fd4b758a5995a9f520d69e9eaef77cf8bed3
petems <p.morsou@gmail.com>
2013-04-11 17:11:50 +0100

    Updated Gemspec

--- lib/capistrano/ext/puppetize/version.rb
+++ lib/capistrano/ext/puppetize/version.rb
@@ -3,7 +3,7 @@ module Capistrano
     module Puppetize
       module Version
         MAJOR = 0
-        MINOR = 3
+        MINOR = 4
         TINY = 1
         STRING= [MAJOR, MINOR, TINY].join('.')
       end
